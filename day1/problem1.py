c6e3dff5e71032a8908123f0b3214930b1c912f3
Jack Travis <72758761+TravioliGit@users.noreply.github.com>
2023-12-02 17:50:02 +0000

    day1problem1

---
+++ day1/problem1.py
@@ -0,0 +1,17 @@
+def main(): 
+    content = open('./files/day1-1.txt', 'r')
+    total = 0
+
+    for line in content:
+        foundNumbers = []
+        for character in line:
+            type = character.isnumeric()
+            if type == True:
+                foundNumbers.append(character)
+        lineTotal = str(foundNumbers[0]) + str(foundNumbers[len(foundNumbers)-1])
+        total = total + int(lineTotal)
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
