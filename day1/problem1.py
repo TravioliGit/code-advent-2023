bc2a90481fb17184903c54ec4128653f0d839a07
Jack Travis <72758761+TravioliGit@users.noreply.github.com>
2023-12-05 13:16:27 +0000

    crikey:

--- day1/problem1.py
+++ day1/problem1.py
@@ -1,17 +1,41 @@
 def main(): 
     content = open('./files/day1-1.txt', 'r')
-    total = 0
+    # total1 = 0
+
+    # for line in content:
+    #     foundNumbers = []
+    #     for character in line:
+    #         type = character.isnumeric()
+    #         if type == True:
+    #             foundNumbers.append(character)
+    #     lineTotal = str(foundNumbers[0]) + str(foundNumbers[len(foundNumbers)-1])
+    #     total1 = total1 + int(lineTotal)
+    # print(total1)
+
+    total2 = 0
+    numbersThatAreWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']  
+    i = 0
 
     for line in content:
-        foundNumbers = []
+        foundNumbers2 = []
+        i = 0
+        print('og line '+line)
+        for word in numbersThatAreWords:
+            line = line.replace(word, str(i))
+            i += 1
+        print('updated line '+line)
+
         for character in line:
             type = character.isnumeric()
             if type == True:
-                foundNumbers.append(character)
-        lineTotal = str(foundNumbers[0]) + str(foundNumbers[len(foundNumbers)-1])
-        total = total + int(lineTotal)
-    print(total)
+                foundNumbers2.append(character)
+        print('all numbers found ',foundNumbers2)
+        lineTotal2 = foundNumbers2[0] + foundNumbers2[len(foundNumbers2)-1]
+        print('total from line to add to running total',lineTotal2)
+        total2 = total2 + int(lineTotal2)
+        print('running total',total2)    
+    print(total2)
 
 
 if __name__ == "__main__":
-    main()
+    main()  
