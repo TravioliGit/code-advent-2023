788c2aa479ba4ff8461c8e37f863ed8cc3b547de
Jack Travis <up938828@myport.ac.uk>
2023-12-06 22:08:28 +0000

    updated so it works in tandem

--- day1/problem1.py
+++ day1/problem1.py
@@ -1,5 +1,6 @@
 def main(): 
     content = open('./files/day1-1.txt', 'r')
+    content2 = open('./files/day1-1.txt', 'r')
     total1 = 0
 
     for line in content:
@@ -10,32 +11,26 @@ def main():
                 foundNumbers.append(character)
         lineTotal = str(foundNumbers[0]) + str(foundNumbers[len(foundNumbers)-1])
         total1 = total1 + int(lineTotal)
-    print(total1)
+    print('total',total1)
 
-    # total2 = 0
-    # numbersThatAreWords = ['oneight', 'twone', 'threeight', 'fiveight', 'sevenine', 'eightwo', 'eighthree', 'nineight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    # numbers = [18, 21, 38, 58, 79, 82, 83, 98, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    total2 = 0
+    numbersThatAreWords = ['oneight', 'twone', 'threeight', 'fiveight', 'sevenine', 'eightwo', 'eighthree', 'nineight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    numbers = [18, 21, 38, 58, 79, 82, 83, 98, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    # for line in content:
-    #     foundNumbers2 = []
-    #     i = 0
-    #     print('og line '+line)
-    #     for word in numbersThatAreWords:
-    #         line = line.replace(word, str(numbers[i]))
-    #         i += 1
-    #     print('updated line '+line)
+    for line2 in content2:
+        foundNumbers2 = []
+        i = 0
+        for word2 in numbersThatAreWords:
+            line2 = line2.replace(word2, str(numbers[i]))
+            i += 1
 
-
-    #     for character in line:
-    #         type = character.isnumeric()
-    #         if type == True:
-    #             foundNumbers2.append(character)
-    #     print('all numbers found ',foundNumbers2)
-    #     lineTotal2 = foundNumbers2[0] + foundNumbers2[len(foundNumbers2)-1]
-    #     print('total from line to add to running total',lineTotal2)
-    #     total2 = total2 + int(lineTotal2)
-    #     print('running total',total2)    
-    # print(total2)
+        for character2 in line2:
+            type2 = character2.isnumeric()
+            if type2 == True:
+                foundNumbers2.append(character2)
+        lineTotal2 = foundNumbers2[0] + foundNumbers2[len(foundNumbers2)-1]
+        total2 = total2 + int(lineTotal2)
+    print('total2',total2)
 
 if __name__ == "__main__":
     main()  
