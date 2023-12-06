7f3593848f718d882216afa06e8a3c9b5b1f3e75
Jack Travis <72758761+TravioliGit@users.noreply.github.com>
2023-12-06 20:00:42 +0000

    ugh

--- day1/problem1.py
+++ day1/problem1.py
@@ -13,28 +13,38 @@ def main():
     # print(total1)
 
     total2 = 0
-    numbersThatAreWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']  
+    numbersThatAreWords = ['zero', 'oneight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'oneight']  
+    line = 'qoneight367gqbpbtbffivetlhrdjgnml2d'
+
     i = 0
+    for word in numbersThatAreWords:
+        newLine = line.replace(word, str(i))
+        line = newLine
+        i += 1
+    print(line)
 
-    for line in content:
-        foundNumbers2 = []
-        i = 0
-        print('og line '+line)
-        for word in numbersThatAreWords:
-            line = line.replace(word, str(i))
-            i += 1
-        print('updated line '+line)
 
-        for character in line:
-            type = character.isnumeric()
-            if type == True:
-                foundNumbers2.append(character)
-        print('all numbers found ',foundNumbers2)
-        lineTotal2 = foundNumbers2[0] + foundNumbers2[len(foundNumbers2)-1]
-        print('total from line to add to running total',lineTotal2)
-        total2 = total2 + int(lineTotal2)
-        print('running total',total2)    
-    print(total2)
+    # for line in content:
+    #     foundNumbers2 = []
+    #     i = 0
+    #     tempLine = ''
+    #     print('og line '+line)
+    #     for word in numbersThatAreWords:
+    #         newLine = line.replace(word, str(i))
+    #         i += 1
+    #         tempLine = newLine
+    #     print('updated line '+tempLine)
+
+    #     for character in tempLine:
+    #         type = character.isnumeric()
+    #         if type == True:
+    #             foundNumbers2.append(character)
+    #     print('all numbers found ',foundNumbers2)
+    #     lineTotal2 = foundNumbers2[0] + foundNumbers2[len(foundNumbers2)-1]
+    #     print('total from line to add to running total',lineTotal2)
+    #     total2 = total2 + int(lineTotal2)
+    #     print('running total',total2)    
+    # print(total2)
 
 
 if __name__ == "__main__":
